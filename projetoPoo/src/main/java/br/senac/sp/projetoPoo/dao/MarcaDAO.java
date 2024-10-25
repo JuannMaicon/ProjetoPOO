0a3104108e3babb2619cc714bbd63b5a7a53de13
JuannMaicon <jmaicon308@gmail.com>
2024-10-25 09:00:56 -0300

    Segunda Aula Dao

--- projetoPoo/src/main/java/br/senac/sp/projetoPoo/dao/MarcaDAO.java
+++ projetoPoo/src/main/java/br/senac/sp/projetoPoo/dao/MarcaDAO.java
@@ -1,5 +1,28 @@
 package br.senac.sp.projetoPoo.dao;
 
-public class MarcaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.senac.sp.projetoPoo.modelo.Marca;
+
+public class MarcaDAO {
+	private Connection conexao;
+	private String sql;
+	private PreparedStatement stmt;
+
+	public MarcaDAO(Connection conexao) {
+
+		this.conexao = conexao;
+	}
+
+	public void inserir(Marca marca) throws SQLException {
+		sql = "insert into marca(nome, logo) values (?,?)";
+		stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, marca.getNome());
+        stmt.setBytes(2, marca.getLogo());
+        stmt.execute();
+        stmt.close();
+
+	}
 }
